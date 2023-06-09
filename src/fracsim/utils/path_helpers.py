e4afbcd4c49be2f8e2aa9bf2037000b4d0a1f10e
Lars Kaiser <71793357+kaiserls@users.noreply.github.com>
2023-06-10 01:08:33 +0200

    Quick and dirty path hack

--- src/fracsim/utils/path_helpers.py
+++ src/fracsim/utils/path_helpers.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import logging
 
 REL_PATH_TO_APPS = "PANDAS_coop/src/app/apps/"
 APP_TEMPLATE = "2d_fracking_saturated/"
@@ -17,7 +18,11 @@ def get_pandas_app_path():
                 )
         repo_path = Path(*cwd_path.parts[0 : i + 1])
         return repo_path
-
+    try:
+        with open("repo_path", "r") as f:
+            repo_path = f.read()
+    except FileNotFoundError:
+        logging.error("Could not find file repo_path. Please create it. Trying to guess the path.")
     repo_path = get_repository_path(REPOSITORY_NAME)
     app_path = repo_path / REL_PATH_TO_APPS / APP_TEMPLATE
     return app_path
