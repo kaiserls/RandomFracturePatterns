0bfd47d9e7f325d4b7db48acb36cd52a12e15456
Lars Kaiser <71793357+kaiserls@users.noreply.github.com>
2023-06-10 20:33:55 +0200

    strip endline

--- src/fracsim/utils/path_helpers.py
+++ src/fracsim/utils/path_helpers.py
@@ -20,8 +20,7 @@ def get_pandas_app_path():
         return repo_path
     try:
         with open("repo_path", "r") as f:
-            repo_path = f.read()
-            repo_path = Path(repo_path)
+            repo_path = Path(f.readline().strip())
     except FileNotFoundError:
         logging.error("Could not find file repo_path. Please create it. Trying to guess the path.")
         repo_path = get_repository_path(REPOSITORY_NAME)
